aa2972ee591585d6a35cea7e3544eb60ee4039ed
Rakesh Sahu <rakeshsahukwd96@gmail.com>
2020-07-17 17:35:05 +0530

    all page design

--- app/src/main/java/com/awizomtech/bastararts/Activity/OrderDetailActivity.java
+++ app/src/main/java/com/awizomtech/bastararts/Activity/OrderDetailActivity.java
@@ -3,12 +3,17 @@ package com.awizomtech.bastararts.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.awizomtech.bastararts.R;
 
 public class OrderDetailActivity extends AppCompatActivity {
-String OrderID,Quantity,OrderNo,OrderDate,TotalAmount,IsPaymentDone,
-        ShippingCharge,AddressLineOne,Status,Landmark,PinCode,Country,State,City,ProductID,ProductCode,ProductName,ProductImage,Description;
+    String OrderID, Quantity, OrderNo, OrderDate, TotalAmount, IsPaymentDone,
+            ShippingCharge, AddressLineOne, Status, Landmark, PinCode, Country, State, City, ProductID, ProductCode, ProductName, ProductImage, Description;
+    TextView Orderno, Orderdate, Shippingcharge, Totalamount, Productname, Productcode, Qty, Address1, Address2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +22,23 @@ String OrderID,Quantity,OrderNo,OrderDate,TotalAmount,IsPaymentDone,
     }
 
     private void Initview() {
+        Button backpress=findViewById(R.id.back);
+        backpress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        Orderno = findViewById(R.id.orderNo);
+        Orderdate = findViewById(R.id.date);
+        Shippingcharge = findViewById(R.id.shippingcharge);
+        Totalamount = findViewById(R.id.productprice);
+        Productname = findViewById(R.id.productname);
+        Productcode = findViewById(R.id.productcode);
+        Qty = findViewById(R.id.productqty);
+        Address1 = findViewById(R.id.addresslineOne);
+        Address2 = findViewById(R.id.AddresslineTwo);
+
         OrderID = getIntent().getExtras().getString("OrderID");
         Quantity = getIntent().getExtras().getString("Quantity");
         OrderNo = getIntent().getExtras().getString("OrderNo");
@@ -36,5 +58,17 @@ String OrderID,Quantity,OrderNo,OrderDate,TotalAmount,IsPaymentDone,
         ProductName = getIntent().getExtras().getString("ProductName");
         ProductImage = getIntent().getExtras().getString("ProductImage");
         Description = getIntent().getExtras().getString("Description");
+        String date = OrderDate.split("T")[0];
+        Orderno.setText(OrderNo.toString());
+        Orderdate.setText( date.toString());
+        Shippingcharge.setText(ShippingCharge.toString());
+        Totalamount.setText(TotalAmount.toString()+" ₹");
+        Productname.setText(ProductName.toString());
+        Productcode.setText(ProductCode.toString());
+        Qty.setText(Quantity.toString());
+        Address1.setText(AddressLineOne.toString()+" "+Landmark.toString());
+        Address2.setText(Country.toString()+" "+State.toString()+" "+City.toString()+" "+PinCode.toString());
+
+
     }
 }
