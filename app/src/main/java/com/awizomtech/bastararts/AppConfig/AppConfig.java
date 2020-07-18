5d192326176edeef9bab458d7aae4cadd0f9ac41
Rakesh Sahu <rakeshsahukwd96@gmail.com>
2020-07-18 13:45:52 +0530

    publish check

--- app/src/main/java/com/awizomtech/bastararts/AppConfig/AppConfig.java
+++ app/src/main/java/com/awizomtech/bastararts/AppConfig/AppConfig.java
@@ -1,9 +1,9 @@
 package com.awizomtech.bastararts.AppConfig;
 
 public class AppConfig {
-       public static final String BASE_URL = "http://192.168.1.7:13641/";
-    public static final String BASE_URL_API = "http://192.168.1.7:13641/api/apps/";
+ /*      public static final String BASE_URL = "http://192.168.1.7:13641/";
+    public static final String BASE_URL_API = "http://192.168.1.7:13641/api/apps/";*/
 
-    /*public static final String BASE_URL = "http://bastararts.jihuzzur.com/";
-    public static final String BASE_URL_API = "http://bastararts.jihuzzur.com/api/apps/";*/
+    public static final String BASE_URL = "http://bastararts.jihuzzur.com/";
+    public static final String BASE_URL_API = "http://bastararts.jihuzzur.com/api/apps/";
 }
